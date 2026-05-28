b625f943a4200a003a3524ecd2bc1300f82d1f5f
szaratep <sebaszarate2005@gmail.com>
2026-05-27 20:24:30 -0500

    [+] Se crea las excepciones y los controles de las mismas de dos maneras, defensiva (getorderbyid, deleteorder) y validacion basada en excepciones en (updateProduct)

--- src/controllers/order.controllers.js
+++ src/controllers/order.controllers.js
@@ -1,3 +1,4 @@
+import mongoose, { MongooseError } from 'mongoose';
 import { dbDeleteOrders, dbGetOrders, dbGetOrdersById, dbInsertOrders, dbUpdateOrders,  } from '../service/order.service.js';
 
 async function getOrder (req, res){
@@ -19,15 +20,20 @@ async function getOrderById(req, res){
     try{
         const id = req.params.idOrder;
 
+        //validacion defensiva: condicionamos antes de que ocurra un error (nunca ocurre)
+        if (!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(400).json({ msg: "El Id es invalido, porfavor verificar, no sea bruto"})
+        }
+
         const data = await dbGetOrdersById(id);
 
-        res.json({
+        res.status(200).json({
             msg: 'Se encontro con exito',
             data : data
         })
     }catch(error){
         console.error(error);
-        res.json({
+        res.status(500).json({
             msg : "Se produjo un error al encontrar tu orden"
         });
     }
@@ -64,6 +70,10 @@ async function updateOrder (req, res){
             data: data
         })
     }catch(error){
+        //validacion de exeception: Manejar cuando ocurre algun error
+        if ( error.name === 'CastError' ){
+            return res.status(400).json({ msg: "No puedo actualizar por que el id es invalido"})
+        }
         console.error(error)
         res.status(500).json({
             msg : "No se logro realizar la actualizacion, intentalo de nuevo"
@@ -74,6 +84,10 @@ async function updateOrder (req, res){
 async function deleteOrder (req, res){
     try{
         const id = req.params.idOrder;
+
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(400).json({ msg: "No se puede eliminar ya que el id es invalido" })
+        }
 
         const data = await dbDeleteOrders(id);
 
