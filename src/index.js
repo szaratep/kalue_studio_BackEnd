00d98b7324d63f7519d19a8e9d45565c021f32b1
katherin castillo <kate.castilloc20@gmail.com>
2026-09-09 18:41:47 -0500

    Registra cambios posterior a resolver conflictos de fusion del remoto al local

--- src/index.js
+++ src/index.js
@@ -24,10 +24,9 @@ dbConect();
 
 //Middelwares
 app.use(express.json()); //habilita la interpretacion de objetos json
-app.use(cors({
-
+app.use(cors(
    // origin: 'http://localhost:4200'
-}));
+));
 
 
 app.use('/uploads', express.static('public/uploads'));
